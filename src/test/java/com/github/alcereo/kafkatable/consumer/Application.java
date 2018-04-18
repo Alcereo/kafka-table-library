b823c9e65e1fa0fc9028942f70dd5663a82ed237
alcereo <ak-91@mail.ru>
2018-04-18 13:51:57 +0300

    Consumer with avro

--- src/test/java/com/github/alcereo/kafkatable/consumer/Application.java
+++ src/test/java/com/github/alcereo/kafkatable/consumer/Application.java
@@ -1,16 +1,18 @@
 package com.github.alcereo.kafkatable.consumer;
 
-import org.apache.kafka.common.serialization.Serdes;
-import org.apache.kafka.streams.KafkaStreams;
-import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.StreamsConfig;
-import org.apache.kafka.streams.kstream.GlobalKTable;
-import org.apache.kafka.streams.kstream.Materialized;
+import io.confluent.kafka.serializers.KafkaAvroDeserializer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import processing.DeviceEvent;
 
+import java.util.Collections;
 import java.util.Properties;
 
 @SpringBootApplication
@@ -18,47 +20,96 @@ import java.util.Properties;
 public class Application {
 
     private static final String TABLE_TOPIC = "longs-table";
+    public static final String TABLE_STORE = "tableStore";
+
+    static final String EVENT_TOPIC = "event-avro-topic";
 
 
     private static final String BROKERS = "192.170.0.3:9092";
-    public static final String TABLE_STORE = "tableStore";
+    private static final String SCHEMA_REGISTRY_URL = "http://192.170.0.6:8081";
 
 
     public static void main(String[] args){
         SpringApplication.run(Application.class, args);
     }
 
-
     @Bean(destroyMethod = "close")
-    public KafkaStreams kafkaStreams(){
+    public KafkaConsumer<String, DeviceEvent> kafkaConsumer(){
 
         Properties config = new Properties();
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BROKERS);
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, "event-consumer-1");
+        config.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
 
-        config.put(StreamsConfig.APPLICATION_ID_CONFIG, "ktable-client-kafka-stream");
-        config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, BROKERS);
+        config.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "100");
+        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
-        config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG,
-                Serdes.String().getClass().getName());
-        config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG,
-                Serdes.Long().getClass().getName());
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+                KafkaAvroDeserializer.class.getName());
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+                KafkaAvroDeserializer.class.getName());
+        config.put("schema.registry.url", SCHEMA_REGISTRY_URL);
+        config.put("specific.avro.reader", "true");
 
-        config.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 100L);
 
-//        ----
+        KafkaConsumer<String, DeviceEvent> consumer = new KafkaConsumer<>(config);
 
-        StreamsBuilder builder = new StreamsBuilder();
+        consumer.subscribe(Collections.singletonList(EVENT_TOPIC));
 
-        GlobalKTable<String, Long> longs_table = builder
-                .globalTable(
-                        TABLE_TOPIC,
-                        Materialized.as(TABLE_STORE)
-                );
+        return consumer;
+    }
 
-        KafkaStreams streams = new KafkaStreams(builder.build(), config);
-        streams.start();
+    @Autowired
+    KafkaConsumer<String, DeviceEvent> consumer;
 
-        return streams;
+    @Autowired
+    EventInMemoryStore store;
+
+    @Scheduled(fixedRate = 100)
+    public void pollMessages(){
+
+        ConsumerRecords<String, DeviceEvent> records = consumer.poll(300);
+
+        records.forEach(
+                record -> {
+                    store.addEvent(record.value());
+                }
+        );
+
+        consumer.commitSync();
 
     }
+
+//    @Bean(destroyMethod = "close")
+//    public KafkaStreams kafkaStreams(){
+//
+//        Properties config = new Properties();
+//
+//        config.put(StreamsConfig.APPLICATION_ID_CONFIG, "ktable-client-kafka-stream");
+//        config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, BROKERS);
+//
+//        config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG,
+//                Serdes.String().getClass().getName());
+//        config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG,
+//                Serdes.Long().getClass().getName());
+//
+//        config.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 100L);
+//
+////        ----
+//
+//        StreamsBuilder builder = new StreamsBuilder();
+//
+//        GlobalKTable<String, Long> longs_table = builder
+//                .globalTable(
+//                        TABLE_TOPIC,
+//                        Materialized.as(TABLE_STORE)
+//                );
+//
+//        KafkaStreams streams = new KafkaStreams(builder.build(), config);
+//        streams.start();
+//
+//        return streams;
+//
+//    }
 
 }
