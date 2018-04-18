15e4fa4e4711f4a54e1f701c20e6fb40e108b8d6
alcereo <ak-91@mail.ru>
2018-04-18 14:25:48 +0300

    Add "new" subproject to test migration

---
+++ kafkalibrary-utils-new/src/test/java/kafkatable/admin/AdminController.java
@@ -0,0 +1,51 @@
+package kafkatable.admin;
+
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.DeleteTopicsResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+@RestController
+@RequestMapping
+@SpringBootApplication
+public class AdminController {
+
+    private static final String BROKERS = "192.170.0.3:9092";
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminController.class, args);
+    }
+
+    @Bean(destroyMethod = "close")
+    public AdminClient adminClient(){
+        Properties config = new Properties();
+        config.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, BROKERS);
+
+        return AdminClient.create(config);
+    }
+
+    @Autowired
+    private AdminClient adminClient;
+
+
+    @DeleteMapping("topics")
+    public void deleteTopic(@RequestBody String name) throws ExecutionException, InterruptedException {
+
+        DeleteTopicsResult deleteTopicsResult = adminClient.deleteTopics(Collections.singletonList(name));
+
+        deleteTopicsResult.all().get();
+    }
+
+
+}
---
+++ kafkalibrary-utils-new/src/test/java/kafkatable/loadgen/EventsGenerator.java
@@ -0,0 +1,99 @@
+package kafkatable.loadgen;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import processing.DeviceEvent;
+
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Component
+@EnableScheduling
+@SpringBootApplication
+public class EventsGenerator {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventsGenerator.class, args);
+    }
+
+    @Bean
+    public RestTemplate template() {
+        return new RestTemplateBuilder ()
+                .rootUri("http://localhost:8080/")
+                .build();
+    }
+
+
+    public EventsGenerator() {
+
+        ExecutorService exec = Executors.newCachedThreadPool();
+
+        for (int i = 0; i < 5; i++) {
+            exec.execute(() -> {
+                RestTemplate build = new RestTemplateBuilder()
+                        .rootUri("http://localhost:8080/")
+                        .build();
+
+                while (!Thread.currentThread().isInterrupted()) {
+
+                    HttpHeaders headers = new HttpHeaders();
+                    headers.setContentType(MediaType.APPLICATION_JSON);
+
+                    generateAndSendEvent(build, headers);
+                }
+            });
+        }
+    }
+
+    @Autowired
+    RestTemplate template;
+
+    AtomicInteger count = new AtomicInteger();
+
+    private String requestSpeed = "line.request.speed";
+
+    @Scheduled(fixedRate=1000)
+    public void rateMetricConsole(){
+        int andSet = count.getAndSet(0);
+
+        System.out.println(andSet);
+    }
+
+    private Random random = new Random();
+
+
+    public void generateAndSendEvent(RestTemplate template, HttpHeaders headers){
+
+        count.getAndIncrement();
+
+        DeviceEvent event = DeviceEvent.newBuilder()
+                .setDeviceId(String.valueOf(random.nextInt(10)))
+                .setComponentId(String.valueOf(random.nextInt(10)))
+                .setEventId(String.valueOf(random.nextInt(10)))
+                .setTimestamp(String.valueOf(System.currentTimeMillis()))
+                .build();
+
+        HttpEntity<String> entity = new HttpEntity<>(event.toString(), headers);
+
+        template.postForEntity(
+                "http://localhost:8080/event",
+                entity,
+                String.class
+        );
+
+    }
+
+}
