7fc375f60c7fe7f194b8ac83b0fbf2ec8a80bf68
alcereo <ak-91@mail.ru>
2018-04-24 14:36:38 +0300

    Add metrics to client. Prepare for remote running.

--- sample-client/src/main/java/com/github/alcereo/kafkatool/sample/client/KafkaConfig.java
+++ sample-client/src/main/java/com/github/alcereo/kafkatool/sample/client/KafkaConfig.java
@@ -3,29 +3,47 @@ package com.github.alcereo.kafkatool.sample.client;
 import com.github.alcereo.kafkatool.KafkaTool;
 import com.github.alcereo.kafkatool.consumer.KtConsumerLoop;
 import com.github.alcereo.kafkatool.topic.KtTopic;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import processing.DeviceBusinessStatus;
 
-import static com.github.alcereo.kafkatool.sample.client.Application.*;
+import java.util.concurrent.ExecutionException;
 
 @Configuration
 public class KafkaConfig {
 
+    @Value("${kafka.topic.business-status.name:device-business-status-table}")
+    private String DEVICE_BUSINESS_STATUS_TABLE;
+
+    @Value("${kafka.topic.business-status.numparts:20}")
+    private Integer businessTopicNumparts;
+
+    @Value("${kafka.brokers}")
+    private String BROKERS;
+
+    @Value("${kafka.registry.url}")
+    private String SCHEMA_REGISTRY_URL;
+
     @Bean
-    public KafkaTool kafkaTool(){
-        return KafkaTool.fromBrokers(BROKERS)
-                .schemaRegistry(SCHEMA_REGISTRY_URL);
+    public KafkaTool kafkaTool(MeterRegistry registry){
+        return KafkaTool
+                .builder()
+                .brokers(BROKERS)
+                .schemaRegistryUrl(SCHEMA_REGISTRY_URL)
+                .meterRegistry(registry)
+                .build();
     }
 
     @Bean
     public KtTopic<Integer, DeviceBusinessStatus> businessTopic(
             KafkaTool kafkaTool
-    ){
+    ) throws ExecutionException, InterruptedException {
         return kafkaTool
                 .topicAvroSimpleTableBuilder(Integer.class, DeviceBusinessStatus.class)
                 .topicName(DEVICE_BUSINESS_STATUS_TABLE)
-                .numPartitions(20)
+                .numPartitions(businessTopicNumparts)
                 .build();
     }
 
